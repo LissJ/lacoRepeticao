2f8a676fa40d359b6a9a3cf19163a55f9772ac80
Liss <lissj014@gmail.com>
2024-05-29 11:26:17 -0300

    eba

---
+++ lacoRepeticao/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace lacoRepeticao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*-----------------------------------------------------------*/
+
+            /*------------------------EXERCICIO 1------------------------*/
+
+            /*Console.WriteLine("Informe um número para ver sua tabuada:");
+            
+            int numero = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine(numero * i);
+            }*/
+
+            /*-----------------------------------------------------------*/
+
+            /*------------------------EXERCICIO 2------------------------*/
+
+            /*Console.WriteLine("Digite um número:");
+
+            int numero = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i <= numero; i += 3)
+            {
+                Console.WriteLine(i);
+            }*/
+
+            /*-----------------------------------------------------------*/
+
+            /*------------------------EXERCICIO 3------------------------*/
+
+            /*for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("Digite um número:");
+
+                int numero = int.Parse(Console.ReadLine());
+
+                if (numero > 100)
+                {
+                    Console.WriteLine("Número especial.");
+                }
+                else
+                {
+                    Console.WriteLine("Número comum.");
+                }
+            }*/
+
+            /*-----------------------------------------------------------*/
+
+            /*------------------------EXERCICIO 4------------------------*/
+
+            /*string resposta;
+            do
+            {
+                Console.WriteLine("Deseja continuar? (s/n)");
+
+                resposta = Console.ReadLine().ToLower();
+
+            } while (resposta == "s");*/
+        }
+    }
+}
